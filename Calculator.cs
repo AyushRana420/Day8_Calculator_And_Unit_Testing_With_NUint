311aafb90ea6e3d8c01829d4edac262ce7ceda26
Ayush <ayushrana419@gmail.com>
2025-07-28 14:16:31 +0530

    Add project files.

---
+++ Calculator.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo__MathLibrary_Testing_using_NUnit
+{
+    public class Calculator
+    {
+        public int Add(int a, int b)//Traditional method
+        {
+            return a + b;
+        }
+        public int Subtract(int a, int b) => a - b; // lambda expression
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+//Steps for writing your first Unit Testing using UNit Testing Framework
+//Step 1: Create a .NET Core Console Application
+//Step 2: Add a class with a method to be tested eg=> mathLibrary
+//Step 3: Add NUnit test project
+// Right click on solution => Add a new project => NUnit Test Project
+// Step 4: Install NUnit, NUnit3TestAdapter and Microsoft.NET.Test.Sdk NuGet packages
+// Right click on the project => Manage NuGet Packages => Search for NUnit and NUnit3TestAdapter
+// Step 5: Write test cases in the test project
+// Step 6: Run the tests
